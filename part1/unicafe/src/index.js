6969d7811dfbcb9e96212a0048529078ff88d2ba
Andrei <adimitrascu.94@gmail.com>
2020-04-08 21:51:43 +0300

    1.6 completedi truly

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,17 +1,51 @@
 import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
+import "./style.css";
 
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const giveFeedback = (type) => {
+    switch(type) {
+      case "good":
+        setGood(good + 1);
+        break;
+      case "neutral":
+        setNeutral(neutral + 1);
+        break;
+      case "bad":
+        setBad(bad + 1);
+        break;
+    }
+  }
+
   return (
     <div>
-      
+      <h2>Give feedback</h2>
+      <div className="btn_container">
+        <Button title="good" className="btn good" handleClick={() => { giveFeedback("good") }}/>
+        <Button title="neutral" className="btn neutral" handleClick={() => { giveFeedback("neutral") }}/>
+        <Button title="bad" className="btn bad" handleClick={() => { giveFeedback("bad") }}/>
+      </div>
+      <Statistics metrics={{good, neutral, bad}} />
+    </div>
+  );
+};
+
+const Statistics = ({metrics}) => {
+  return (
+    <div>
+      <h3>Statistics</h3>
+      <p>good {metrics.good}</p>
+      <p>neutral {metrics.neutral}</p>
+      <p>bad {metrics.bad}</p>
     </div>
   );
 }
+
+const Button = ({title, handleClick, ...rest}) => (<button {...rest} onClick={handleClick}>{title}</button>);
 
 ReactDOM.render(
   <React.StrictMode>
