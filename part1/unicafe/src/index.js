fe5713c1db1d3a8dbdad1c4628b393b56bd6ba63
Andrei <adimitrascu.94@gmail.com>
2020-04-08 21:29:33 +0300

    1.6 completed

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,69 +2,15 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  const [name, setName] = useState("Guest");
-  const [age, setAge] = useState(18);
-  const [friends, setFriends] = useState([]);
-  const [nameInput, setInput] = useState("");
-
-  const handleClick = (val) => () => setAge(age + val);
-
-  const handleInput = (e) => {
-    setInput(e.target.value);
-    setName(e.target.value);
-  }
-
-  const handleAddFriend = (friend) => {
-    setFriends(friends.concat(friend));
-  }
-
-  const handleClickChangeName = (e) => {
-    setInput("");
-  }
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <h1>Hello {name}</h1>
-      <input type="text" name="name" value={nameInput} placeholder="Change your name" onChange={handleInput}/>
-      <button onClick={handleClickChangeName}>Change</button>
-      <h3>You are {age} years old.</h3>
-      <Accessor age={age}/>
-      <button onClick={handleClick(1)}>be older</button>
-      <button onClick={handleClick(-1)}>be younger</button>
-      <FriendList friends={friends} handleAddFriend={handleAddFriend}/>
+      
     </div>
   );
-};
-
-const FriendList = ({friends, handleAddFriend}) => {
-  const [addInputValue, setAddInputValue] = useState("");
-
-  const handleInputChange = (e) => {
-    setAddInputValue(e.target.value);
-  }
-
-  return (
-    <div>
-      <h3>Your current frient list:</h3>
-      <ul>
-        {friends.map((friend, idx) => (<li key={idx}>{friend}</li>))}
-      </ul>
-      <input type="text" value={addInputValue} onChange={handleInputChange} placeholder="Add friend"/>
-      <button onClick={
-        (e) => { 
-          handleAddFriend(addInputValue); 
-          setAddInputValue("");
-        }
-      }>Add</button>
-    </div>
-  );
-}
-
-const Accessor = ({age}) => {
-  if (age < 18) {
-    return (<h1 style={{backgroundColor: "red"}}>You are too young ({age}). Come back when you are older</h1>);
-  }
-  return null;
 }
 
 ReactDOM.render(
